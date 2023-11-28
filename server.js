9ba1435313f5faa97306b95027c8959a7a5aef44
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-11-28 17:51:25 +0000

    Updated ENV variables

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
   
-  
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-flexible-66906',
-    user : 'devinmcdaniel',
-    password : '',
-    database : 'smartbrain'
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 

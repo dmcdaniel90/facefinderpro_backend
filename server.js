31d1565b1ee5adc50d2bc5b9d3c35762d0562780
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-11-28 18:08:54 +0000

    server update

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
+
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
   
 const db = knex({
   client: 'pg',
@@ -30,6 +40,20 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.get('/profile/:id', (req, res) => {profile.handleGetProfile(req, res, db)})
 app.put('/image', (req, res) => { image.handleImageSubmit(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App running on port ${process.env.PORT}`);

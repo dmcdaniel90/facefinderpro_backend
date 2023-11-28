81912ddf864765940deac2a58886287bd62c4306
dmcdaniel90 <dmcdaniel9@gmail.com>
2023-11-28 17:30:42 +0000

    Added postgres

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-flexible-66906',
     user : 'devinmcdaniel',
     password : '',
     database : 'smartbrain'

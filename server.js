02bee88a596459b2270c0c8405ab3aec07dfd81f
dmcdaniel90 <dmcdaniel9@gmail.com>
2024-02-14 11:47:15 +0000

    init commit

--- server.js
+++ server.js
@@ -10,11 +10,24 @@ const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
   
+/* //! For production
 const db = knex({
   client: 'pg', 
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {rejectUnauthorized: false},
+  }
+});
+*/
+
+//! For development
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'devinmcdaniel',
+    password: '',
+    database: 'smartbrain'
   }
 });
 

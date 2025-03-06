46bc8e72c73f11d8c9996b2054e8f0f08d80efc5
Devin McDaniel <devinmcdaniel@Devins-MacBook-Pro.local>
2025-03-06 18:55:12 +0000

    Reconfigured CORS

--- server.js
+++ server.js
@@ -33,11 +33,6 @@ const db = knex({
 app.use(express.json());
 app.use(helmet());
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get("/", (req, res) => {
   res.json("Success");
